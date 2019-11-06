39a9b7f3af5616719e17872a13e8f2da01d2842c
Maham Shahid <maham.shahid@zenhomes.de>
2019-11-06 13:01:35 +0500

    Added Readme + styling changes for Vue app

---
+++ README.md
@@ -0,0 +1,5 @@
+- Consumer
+- Micro-Apps (overview)
+- Routes
+    - explanation of each route and what app it triggers
+- Explanation of structuring of each micro-app
--- src/vue/main.vue
+++ src/vue/main.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-      <h1>Hello from Vue</h1>
+  <div class="h-100 w-100 py-1 px-2 mx-3" style="background-color: #ffcdcd">
+      <h1>
+        This is just a Vue App which doesn't do much
+      </h1>
   </div>
 </template>
